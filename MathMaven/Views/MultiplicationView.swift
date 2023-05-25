cbc6f8e020c2d902a4dcec60519b0a1c1b646b56
Justin Hui <jhui@lcs.on.ca>
2023-05-25 16:32:34 -0400

    Added a first value, second value, and sets the answerCorrect and the correctRepsonse as false in the multiplication view

--- MathMaven/Views/MultiplicationView.swift
+++ MathMaven/Views/MultiplicationView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct MultiplicationView: View {
+    
+    //MARK: Stored Properties
+    
+    //Numbers for the question
+    @State var firstValue = Int-.random(in: 1...72)
+    @State var secondValue = Int.random(in: 1...72)
+
+    //Holds the input by the user
+    @State var input = ""
+    
+    //indicates if the answer has been checked
+    @State var answerChecked = false
+    
+    //Inidicate if the answer is correct
+    @State var answerCorrect = false
+    
+    //MARK: Computed Properties
+    
+    //The correct answer
+    @State var correctResponse: Int {
+        return firstValue + secondValue
+    }
+    
+    //User interface
     var body: some View {
         VStack {
             Text("Multiplication")
             Text(Operation.multiplication.rawValue)
         }
-        .font(Font.custom("SF Pro", size: 64))
     }
 }
 
